874f567cbd42f7925f8efde54093448c08842f84
Devang Bramhakshatriya <devangbramhakshtriya@gmail.com>
2023-02-23 03:20:27 +0530

    File Added

--- fashion_hub/src/App.js
+++ fashion_hub/src/App.js
@@ -2,29 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { ChakraProvider } from '@chakra-ui/react'
+import Home from './Components/Home/Pages/Home';
 
 
 function App() {
   return (
-    <ChakraProvider>
+    
       <div className="App">
         <Navbar/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Home/>
       </div>
-    </ChakraProvider>
+    
   );
 }
 
