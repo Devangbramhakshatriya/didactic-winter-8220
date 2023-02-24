8a9e86a7add24818407c84e101bb9a89d4f1abf0
Anish-Jha <anishj545@gmail.com>
2023-02-25 01:43:37 +0530

    file

--- fashion_hub/src/Components/Product/ProductCard.jsx
+++ fashion_hub/src/Components/Product/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import color from './image/color.png'
 export default function ProductCard({product}) {
   return (
     <div style={{width:"315px",height:"529px",overflow:"hidden",textAlign:"left",
@@ -9,9 +9,10 @@ export default function ProductCard({product}) {
       <Link to={`/product/${product.id}`}>
       <img style={{width:"295px", height:"395px",margin:"auto"}} src={product.image} alt={product.name}/>
       </Link>
-      <small>{product.subhead}</small>
-      <p style={{fontFamily:"SFHeadingFont", fontWeight:"500", fontSize:"14px"}}>{product.name}</p>
-      <p>Rs.{product.price}</p>
+      <small style={{fontSize:"11px", color:"grey"}}>{product.subhead}</small>
+      <p style={{fontFamily:"Helvetica", fontWeight:"600", fontSize:"12px", color:"#000000",lineHeight:"21px",paddingBottom:"5px"}}>{product.name}</p>
+      <p style={{fontWeight:"400", fontSize:"14px", color:"#000000",lineHeight:"21px",paddingBottom:"5px"}}>Rs. {product.price}</p>
+      <img style={{cursor:"pointer"}} src={color} alt="" />
       <Link to={`/product/${product.id}/edit`}>
         <button>Edit</button>
       </Link>
--- fashion_hub/src/Pages/Product.jsx
+++ fashion_hub/src/Pages/Product.jsx
@@ -1,28 +1,31 @@
-import { Heading } from '@chakra-ui/react'
+import { Heading, Image } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Filter from '../Components/Product/Filter'
 import Navbar from '../Components/Product/Navbar'
 import ProductList from '../Components/Product/ProductList'
-import Sort from '../Components/Product/Sort'
+import logo from '../Components/Product/image/row.png'
 
 export default function Product() {
   return (
-    <div>
-      <Navbar/>
+    // whole container
+    <>
       <div>
-        <Heading style={{fontFamily:"Helvetica", fontSize:"42px", textAlign:"center",fontWeight:"300"}}>MEN ALL COTHING</Heading>
-          <div>
+        <Heading style={{fontFamily:"Helvetica", fontSize:"42px", textAlign:"center",fontWeight:"500",margin:"30px",lineHeight:"28px",
+          textTransform:"uppercase"}}>MEN ALL CLOTHING</Heading>
+          <div style={{fontWeight:"500", fontSize:"14px",margin:"-10px 0px 60px 0px"}}>
              <Link to='/'>Home</Link> {">"} Men all cothing
           </div>
-      </div>
-      <div style={{display:"flex", float:"left"}}>
+      </div >
+
+      {/* sort and filter container */}
+      <div  style={{ display:"flex",paddingLeft:"3.2%",width:"100%"}}>
         <Filter/>
-        <Sort/>
+        <img style={{marginLeft:"63%", cursor:"pointer"}} src={logo} alt=''/>
       </div>
       <div>
       <ProductList/>
       </div>
-    </div>
+    </>
   )
 }
