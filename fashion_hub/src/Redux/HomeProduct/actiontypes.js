faeba93b3a5d39c97010f6f5a1deb91ed7835f58
Devang bramhakshatriya <107463038+Devangbramhakshatriya@users.noreply.github.com>
2023-02-25 10:10:01 +0530

    Delete fashion_hub directory

--- fashion_hub/src/Redux/HomeProduct/actiontypes.js
+++
@@ -1,3 +0,0 @@
-export const Home_REQUEST='Home_REQUEST';
-export const Home_FAILURE='Home_FAILURE';
-export const GET_Home_SUCCESS='GET_Home_SUCCESS';
--- fashion_hub/src/store.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, legacy_createStore ,combineReducers} from "redux";
-import thunk from "redux-thunk";
-import { reducer as product} from "./Redux/ProductRedux/reducer"
-import {reducer as homeReducer} from "./Redux/HomeProduct/reducer"
-
-const rootReducer = combineReducers({product, homeReducer})
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
