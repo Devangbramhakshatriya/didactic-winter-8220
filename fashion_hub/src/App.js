9fa7f22ecbac2ad3082b4f9335090ca01e777d25
Kartikey Mishra <kartikmishra686@gmail.com>
2023-02-26 16:00:54 +0530

    Mergering to main

--- fashion_hub/src/App.js
+++ fashion_hub/src/App.js
@@ -7,28 +7,28 @@ import MainRoutes from './Pages/MainRoutes';
 import Footer from './Components/Footer/Footer';
 import Login from './Components/Login/SignIn';
 import Signup from './Components/Signup/Signup';
-<<<<<<< HEAD
+
 import Loginn from './Components/Login/SignIn';
-=======
+
 import Loginn from './Components/Login/Login';
 import Navbar from './Components/Product/Navbar';
->>>>>>> a7a700885c7e9d9fceeb329bbcd2d6e72ac1c542
+
 // import PaymentPage from './Pages/PaymentPage'
 
 
 function App() {
   return (
       <div className="App">
-<<<<<<< HEAD
+
         {/* <Navbar/>
         <Home/> */}
         <Login/>
         {/* <Signup/> */}
-=======
+
         {/* <Navbar/> */}
         <Navbar/>
         {/* <Login/> */}
->>>>>>> a7a700885c7e9d9fceeb329bbcd2d6e72ac1c542
+
         {/* <Admin/>
         <Product/> */}
         
