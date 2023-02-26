4af0ffe045e335533e1472fae2aad58660a904ec
Devang Bramhakshatriya <devangbramhakshtriya@gmail.com>
2023-02-26 16:00:56 +0530

    FILE

--- fashion_hub/src/Pages/CartCard.jsx
+++ fashion_hub/src/Pages/CartCard.jsx
@@ -1,22 +1,27 @@
 import { Box, Button, Image, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
-function CartCard(){
+export const total=[];
+function CartCard({data}){
     const [count,setCount]=useState(1)
     const handleCount=(val)=>{
+       
         setCount(count+val)
+    
     }
+total.push(count*data.price);
+    // console.log(data.image)
     return(
         <Box display={["","flex","flex","flex","flex","flex"]} w="95%" m="auto" justifyContent="space-between" borderTop="1px solid gray" borderBottom="1px solid gray" fontFamily="sans-serif" alignContent="center" alignItems="center">
                 <Box w={["50%","30%","30%","35%","35%","40%"]}  m="auto">
                     <Text fontWeight="bold">Product</Text>
                     <Box display="flex" gap="10px">
                         <Box w={["90%","60%","50%","25%","20%","20%"]}>
-                            <Image src="https://cdn.shopify.com/s/files/1/0677/1464/6315/products/Flying_1_011c3e91-14bb-46c2-ab89-4df8f5f36041.png?v=1677044552&"/>
+                            <Image src={data.image}/>
                         </Box>
                         <Box m="auto">
-                            <Text>Heading</Text>
-                            <Text>Description</Text>
+                            <Text>{data.name}</Text>
+                            <Text>{data.subhead}</Text>
                             <Button>Remove</Button>
                         </Box>
                     </Box>
@@ -24,7 +29,7 @@ function CartCard(){
 
                 <Box>
                     <Text fontWeight="bold">Price</Text>
-                    <Text>Rs. 2252</Text>
+                    <Text>{data.price}</Text>
                 </Box>
 
 
@@ -33,15 +38,17 @@ function CartCard(){
                     <Box display="flex" border="1px solid gray" bg="none">
                         <Button bg="none" onClick={()=>handleCount(1)}>+</Button>
                         <Text  m="auto">{count}</Text>
-                        <Button bg="none" disabled={count==1} onClick={()=>handleCount(-1)}>-</Button>
+                        <Button bg="none" disabled={count='1'} onClick={()=>handleCount(-1)}>-</Button>
                     </Box>
                 </Box>
 
                 <Box fontWeight="bold">
                     <Text >Total</Text>
-                    <Text>Rs.2315</Text>
+                    <Text>{count*data.price}</Text>
+                    
                 </Box>
         </Box>
+        
     )
 }
 export default CartCard;
